e2a942beb4c79787146168abceea71f1f96cafa8
Ashif Hassan <ashifhassan1996@gmail.com>
2025-05-08 13:05:09 +0530

    Add implementation of bubble sort in JavaScript

--- algorithms/bubble-sort/bubbleSort.js
+++ algorithms/bubble-sort/bubbleSort.js
@@ -1,17 +1,19 @@
-let arr1 = [11, 33, 5, 7, 9, 112, 13, 15, 17, 119, 22, 25, 28, 3, 37, 40, 43, 46, 50];
+let arr1 = [
+  11, 33, 5, 7, 9, 112, 13, 15, 17, 119, 22, 25, 28, 3, 37, 40, 43, 46, 50,
+];
 
 const bubbleSort = (arr) => {
-	for (let i = 0; i < arr.length - 1; i++) {
-		for (let j = 0; j < arr.length - 1 - i; j++) {
-			if (arr[j] > arr[j + 1]) {
-				let temp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = temp;
-			}
-		}
-	}
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
 
-	return arr;
+  return arr;
 };
 
 const result = bubbleSort(arr1);
