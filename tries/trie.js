b16417368f1ff8b3a17f2bc8f90b17c77d4a457a
Ashif Hassan <ashifhassan1996@gmail.com>
2025-08-01 10:21:30 +0530

    Update trie implementation

--- tries/trie.js
+++ tries/trie.js
@@ -30,10 +30,7 @@ class Trie {
     let node = this.root;
 
     for (const char of word) {
-      if (!node.children[char]) {
-        return false;
-      }
-
+      if (!node.children[char]) return false;
       node = node.children[char];
     }
 
@@ -44,10 +41,7 @@ class Trie {
     let node = this.root;
 
     for (const char of prefix) {
-      if (!node.children[char]) {
-        return false;
-      }
-
+      if (!node.children[char]) return false;
       node = node.children[char];
     }
 
