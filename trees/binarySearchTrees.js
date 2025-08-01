ea5dde401779b45f8a0da79458b6b953053af69d
Ashif Hassan <ashifhassan1996@gmail.com>
2025-08-01 09:53:28 +0530

    Update binary search tree implementation

--- trees/binarySearchTrees.js
+++ trees/binarySearchTrees.js
@@ -1,3 +1,5 @@
+// Binary Search Tree implementation
+
 class TreeNode {
   constructor(value) {
     this.value = value;
@@ -6,7 +8,7 @@ class TreeNode {
   }
 }
 
-class BinarySearchTree {
+class BST {
   constructor() {
     this.root = null;
   }
@@ -14,26 +16,34 @@ class BinarySearchTree {
   insert(value) {
     const newNode = new TreeNode(value);
 
-    if (!this.root) this.root = newNode;
-    else this.insertNode(this.root, newNode);
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
   }
 
   insertNode(node, newNode) {
     if (newNode.value < node.value) {
-      if (!node.left) node.left = newNode;
-      else this.insertNode(node.left, newNode);
+      if (!node.left) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
     } else {
-      if (!node.right) node.right = newNode;
-      else this.insertNode(node.right, newNode);
+      if (!node.right) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
     }
   }
 
-  // 1. Traversals
   inOrderTraversal(node = this.root) {
     if (!node) return;
 
-    this.inOrderTraversal(node.left);
     console.log(node.value);
+    this.inOrderTraversal(node.left);
     this.inOrderTraversal(node.right);
   }
 
@@ -57,7 +67,36 @@ class BinarySearchTree {
     return result;
   }
 
-  // 2. Value Exists
+  deleteNode(value, node = this.root) {
+    if (!node) return null;
+
+    if (value < node.value) {
+      node.left = this.deleteNode(value, node.left);
+    } else if (value > node.value) {
+      node.right = this.deleteNode(value, node.right);
+    } else {
+      if (!node.left && !node.right) return null;
+
+      if (!node.right) return node.left;
+
+      if (!node.left) return node.right;
+
+      let minRight = this.findMinNode(node.right);
+      node.value = minRight.value;
+      node.right = this.deleteNode(minRight.value, node.right);
+    }
+
+    return node;
+  }
+
+  findMinNode(node) {
+    while (node.left) {
+      node = node.left;
+    }
+
+    return node;
+  }
+
   isValueExists(value, node = this.root) {
     if (!node) return false;
 
@@ -68,33 +107,6 @@ class BinarySearchTree {
       : this.isValueExists(value, node.right);
   }
 
-  // 3. Delete Node
-  delete(value, node = this.root) {
-    if (!node) return null;
-
-    if (value < node.value) {
-      node.left = this.delete(value, node.left);
-    } else if (value > node.value) {
-      node.right = this.delete(value, node.right);
-    } else {
-      if (!node.left && !node.right) return null;
-      if (!node.left) return node.right;
-      if (!node.right) return node.left;
-
-      let minRight = this.findMinNode(node.right);
-      node.value = minRight.value;
-      node.right = this.delete(minRight.value, node.right);
-    }
-
-    return node;
-  }
-
-  findMinNode(node) {
-    while (node.left) node = node.left;
-    return node;
-  }
-
-  // 4. Find Closest Value
   findClosestValue(target, node = this.root, closest = this.root?.value) {
     if (!node) return closest;
 
@@ -106,36 +118,26 @@ class BinarySearchTree {
       ? this.findClosestValue(target, node.left, closest)
       : this.findClosestValue(target, node.right, closest);
   }
-
-  // 5. Is Valid BST
-  isValidBST(node = this.root, min = -Infinity, max = Infinity) {
-    if (!node) return true;
-
-    if (node.value <= min || node.value >= max) return false;
-
-    return (
-      this.isValidBST(node.left, min, node.value) &&
-      this.isValidBST(node.right, node.value, max)
-    );
-  }
 }
 
-// Example usage
-const bst = new BinarySearchTree();
+const bst = new BST();
 
-[10, 15, 11, 7, 3, 9, 18].forEach((value) => bst.insert(value));
+bst.insert(15);
+bst.insert(5);
+bst.insert(20);
+bst.insert(10);
+bst.insert(25);
 
-console.log("InOrderTraversal:");
 bst.inOrderTraversal();
 
-console.log("PreOrderTraversal:", bst.preOrderTraversal());
-console.log("PostOrderTraversal:", bst.postOrderTraversal());
+console.log(bst.preOrderTraversal());
 
-console.log("Does value 7 exist?:", bst.isValueExists(7));
+console.log(bst.postOrderTraversal());
 
-bst.delete(7);
-console.log("Tree after deleting 7:");
+console.log(bst.isValueExists(9));
+
+console.log(bst.findClosestValue(17));
+
+bst.deleteNode(20);
+
 bst.inOrderTraversal();
-
-console.log("Closest value to 2:", bst.findClosestValue(2));
-console.log("Is valid BST:", bst.isValidBST());
