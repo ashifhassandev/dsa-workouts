3c55bf938733a439be9b7cc771982de4b9535e46
Ashif Hassan <ashifhassan1996@gmail.com>
2025-08-03 12:49:47 +0530

    Remove curly braces from if condition.

--- algorithms/linear-search/linearSearch.js
+++ algorithms/linear-search/linearSearch.js
@@ -4,9 +4,7 @@ let arr1 = [
 
 const linearSearch = (arr, target) => {
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === target) {
-      return i;
-    }
+    if (arr[i] === target) return i;
   }
 
   return -1;
